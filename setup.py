22fe4fb3ea2d015ed2f661c1060feec9693cb283
Adam Brown <adam.brown@evbane.com>
2014-12-29 17:36:13 +0800

    Only return 0th element for request_sample (singular).

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='SESAR-Web-Services-Library',
-      version='0.6',
+      version='0.7',
       description='SESAR web services library for IGSN management.',
       url='https://github.com/Adam-Brown/SESAR-Web-Services-Lib',
       author='Adam David Brown',
